cf73629095ad302d395b2c21da5f41c8b0d847c1
robert.makyla <robert.makyla@gmail.com>
2016-09-26 22:32:32 +0200

    adding test cases for akk Http/POST

--- src/test/scala/PokemonServiceSpec.scala
+++ src/test/scala/PokemonServiceSpec.scala
@@ -1,19 +1,50 @@
 
 import akka.http.scaladsl.model.ContentTypes._
 import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{FreeSpec, MustMatchers}
+import pokemonGo.Model.Pokemon
 import pokemonGo.PokemonService
 
-class PokemonServiceSpec extends FlatSpec with Matchers with ScalatestRouteTest {
+class PokemonServiceSpec extends FreeSpec with MustMatchers with ScalatestRouteTest {
 
-  private val service = new PokemonService {}
-
-  "service" should "show empty list" in {
+  "shows empty list of pokemons at Http/GET" in new Fixture {
     Get(s"/pokemons") ~> service.route ~> check {
-      status shouldBe OK
-      contentType shouldBe `text/plain(UTF-8)`
-      responseAs[String] shouldBe ""
+      status mustBe OK
+      contentType mustBe `text/plain(UTF-8)`
+      responseAs[String] mustBe ""
     }
   }
+
+  "accepts a pokemon with Http/POST and show it at Http/GET" in new Fixture {
+    val name = "monster"
+
+    val jsonRequest = s"{\"name\": \"${name}\"}".getBytes
+
+    val postRequest = HttpRequest(
+      HttpMethods.POST,
+      uri = "/pokemons",
+      entity = HttpEntity(`application/json`, jsonRequest)
+    )
+
+    postRequest ~> service.route ~> check {
+      status mustBe OK
+      contentType mustBe `text/plain(UTF-8)`
+      responseAs[String] mustBe s"Caught pokemon monster!!!"
+    }
+
+    service.list mustBe Pokemon(name) :: Nil
+
+    Get(s"/pokemons") ~> service.route ~> check {
+      status mustBe OK
+      contentType mustBe `text/plain(UTF-8)`
+      responseAs[String] mustBe name
+    }
+  }
+
+  class Fixture {
+    val service = new PokemonService {}
+  }
+
 }
